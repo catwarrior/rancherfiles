54d25f57b8f65362c1c2bc08971f101501d88acf
catwarrior <lishufan00@aliyun.com>
2017-05-12 09:39:02 +0800

    Create rancher-cleanup.sh

---
+++ rancher-cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+docker rm -f $(docker ps -aq)
+
+rm -rf /var/etcd/
+
+for m in $(tac /proc/mounts | awk '{print $2}' | grep /var/lib/kubelet);do
+    umount $m || true
+done
+
+rm -rf /var/lib/kubelet/
+
+for m in $(tac /proc/mounts | awk '{print $2}' | grep /var/lib/rancher);do
+    umount $m || true
+done
+
+rm -rf /var/lib/rancher/
+
+rm -rf /run/kubernetes/
+
+docker volume rm $(docker volume ls -q)
+
+docker ps -a
+docker volume ls
