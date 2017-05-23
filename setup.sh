fe46f408ad03ef84b051456cb6e369e61386c76a
catwarrior <lishufan00@aliyun.com>
2017-05-23 15:45:48 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-mkdir hello-123
+mkdir /etc/docker
+touch /etc/docker/daemon.json
+echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
+systemctl enable docker.service
+systemctl restart docker.service
