a03dc6d7b2f91ed4636e9bcbbc951935dcee6d9d
catwarrior <lishufan00@aliyun.com>
2017-05-24 17:18:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,3 @@ touch /etc/docker/daemon.json
 echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
 systemctl enable docker.service
 systemctl restart docker.service
-docker run -d --restart=unless-stopped -p 8080:8080 rancher/server
