149c917a650147f644d20db02d44c3ccf56fa39e
catwarrior <lishufan00@aliyun.com>
2017-05-25 10:11:34 +0800

    Update azure-rancher-vnet.sh

--- azure-rancher-vnet.sh
+++ azure-rancher-vnet.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 az group create -n vmssrg3 -l chinanorth
-#az network vnet create -g vmssrg3 -n vmssvnet --address-prefix 10.0.0.0/16 --subnet-name vmssvnetsub1 --subnet-prefix 10.0.0.0/24
 
 az vm create -g vmssrg3 -n ranchermaster \
 --authentication-type password --admin-username myadmin --admin-password Admin@1234567890 \
@@ -11,19 +10,19 @@ az vm create -g vmssrg3 -n ranchermaster \
 --public-ip-address "" \
 --no-wait
 
-az vmss create --name rancher3 --resource-group vmssrg3 --location chinanorth --instance-count 2 --vm-sku Standard_A3 \
---authentication-type password --admin-username myadmin --admin-password Admin@1234567890 \
---use-unmanaged-disk --storage-sku Standard_LRS \
---image SUSE:SLES:12-SP2:2017.03.20 --output table \
---subnet /subscriptions/96e96992-4c36-4b58-a68f-ecbfb1f1b79e/resourceGroups/xmbafcmcnbeinfvnt001rg/providers/Microsoft.Network/virtualNetworks/XMBAFCMCNINFVNT001RG/subnets/XMBAFCMCNINFS03DB \
---load-balancer "" --public-ip-address ""
-
 az vm extension set \
   --resource-group vmssrg3 \
   --vm-name ranchermaster \
   --name customScript \
   --publisher Microsoft.Azure.Extensions \
   --settings '{"fileUris": ["https://raw.githubusercontent.com/catwarrior/rancherfiles/master/setup.sh","https://raw.githubusercontent.com/catwarrior/rancherfiles/master/rancher-master.sh"],"commandToExecute": "./setup.sh & ./rancher-master.sh"}'
+
+az vmss create --name rancher3 --resource-group vmssrg3 --location chinanorth --instance-count 2 --vm-sku Standard_A3 \
+--authentication-type password --admin-username myadmin --admin-password Admin@1234567890 \
+--use-unmanaged-disk --storage-sku Standard_LRS \
+--image SUSE:SLES:12-SP2:2017.03.20 --output table \
+--subnet /subscriptions/96e96992-4c36-4b58-a68f-ecbfb1f1b79e/resourceGroups/xmbafcmcnbeinfvnt001rg/providers/Microsoft.Network/virtualNetworks/XMBAFCMCNINFVNT001RG/subnets/XMBAFCMCNINFS03DB \
+--load-balancer "" --public-ip-address ""
 
 az vmss extension set '
   --resource-group vmssrg3 `
