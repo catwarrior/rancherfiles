82b2680c312a022ae98a0e1d11a9f22367b754ac
catwarrior <lishufan00@aliyun.com>
2017-05-25 14:13:05 +0800

    Update azure-rancher-vnet.sh

--- azure-rancher-vnet.sh
+++ azure-rancher-vnet.sh
@@ -31,5 +31,4 @@ az vmss extension set \
   --publisher Microsoft.Azure.Extensions \
   --settings '{"fileUris": ["https://raw.githubusercontent.com/catwarrior/rancherfiles/master/setup.sh, "https://raw.githubusercontent.com/catwarrior/rancherfiles/master/rancher-slaves.sh"],"commandToExecute": "./setup.sh & ./rancher-slave.sh http://172.21.1.196:8080/v1/scripts/3C99474E1D679EA93E60:1483142400000:qyqummPGyQLxnuEaA0MjnFrs"}'
 
-az vm extension set --resource-group vmssrg3 --vm-name vmmaster --name customScript --publisher Microsoft.Azure.Extensions --settings ./script-config.json --verbose --debug
-az vmss extension set --resource-group vmssrg3 --vmss-name rancher3 --name customScript --publisher Microsoft.Azure.Extensions --settings ./script-config.json --verbose --debug
+az vmss update -n rancher3 -g vmssrg3
