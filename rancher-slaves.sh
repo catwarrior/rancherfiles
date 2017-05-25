1265ab1170fc4fe4a44ede19f392910846d093b4
catwarrior <lishufan00@aliyun.com>
2017-05-25 15:15:32 +0800

    Update rancher-slaves.sh

--- rancher-slaves.sh
+++ rancher-slaves.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 mkdir -p /etc/docker
 touch /etc/docker/daemon.json
 echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
