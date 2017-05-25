b2da866fe6744c5db9dddb8d96035829f3bc0d78
catwarrior <lishufan00@aliyun.com>
2017-05-25 15:14:25 +0800

    Update rancher-master.sh

--- rancher-master.sh
+++ rancher-master.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+mkdir -p /etc/docker
+touch /etc/docker/daemon.json
+echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
+systemctl enable docker.service
+systemctl restart docker.service
+## start rancher master
 docker run -d --restart=unless-stopped -p 8080:8080 rancher/server:stable
