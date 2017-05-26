d7806760757e61ff45c0088b125136b04029cfab
catwarrior <lishufan00@aliyun.com>
2017-05-26 15:12:02 +0800

    Update rancher-slaves.sh

--- rancher-slaves.sh
+++ rancher-slaves.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /etc/docker
 touch /etc/docker/daemon.json
-echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
+echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"], \"storage-driver\": \"overlay2\" } > /etc/docker/daemon.json
 systemctl enable docker.service
 systemctl restart docker.service
 ## add nodes
