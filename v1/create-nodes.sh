346f94dc59ca154598289bc3f6e9e8852fc4ca47
catwarrior <lishufan00@aliyun.com>
2017-05-26 16:16:51 +0800

    Update create-nodes.sh

--- v1/create-nodes.sh
+++ v1/create-nodes.sh
@@ -2,6 +2,8 @@
 az vmss create --name rancher3 --resource-group vmssnodesrg  --location chinanorth --instance-count 2 --vm-sku Standard_A3 \
 --authentication-type password --admin-username myadmin --admin-password Admin@1234567890 \
 --use-unmanaged-disk --storage-sku Standard_LRS \
+# auto scale
+--upgrade-policy-mode Automatic \
 --image SUSE:SLES:12-SP2:2017.03.20 --output table \
 --subnet /subscriptions/96e96992-4c36-4b58-a68f-ecbfb1f1b79e/resourceGroups/xmbafcmcnbeinfvnt001rg/providers/Microsoft.Network/virtualNetworks/XMBAFCMCNINFVNT001RG/subnets/XMBAFCMCNINFS03DB \
 --load-balancer "" --public-ip-address ""
