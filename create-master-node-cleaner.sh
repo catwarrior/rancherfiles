17c5b33ac222a3b76371670ddacbac0776791d89
catwarrior <lishufan00@aliyun.com>
2017-05-26 10:37:25 +0800

    Delete create-master-node-cleaner.sh

--- create-master-node-cleaner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-az vm extension set \
-  --resource-group vmssrg3 \
-  --vm-name ranchermaster \
-  --name customScript1 \
-  --publisher Microsoft.Azure.Extensions \
-  --settings '{"fileUris": ["https://raw.githubusercontent.com/catwarrior/rancherfiles/master/rancher-master.sh"],"commandToExecute": "./rancher-master.sh"}'
