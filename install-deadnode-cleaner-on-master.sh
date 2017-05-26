d5bc6317e07daa64267289155bef02502a6091ad
catwarrior <lishufan00@aliyun.com>
2017-05-26 10:47:46 +0800

    Update install-deadnode-cleaner-on-master.sh

--- install-deadnode-cleaner-on-master.sh
+++ install-deadnode-cleaner-on-master.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+az vm extension set \
+  --resource-group vmssrg3 \
+  --vm-name ranchermaster \
+  --name customScript \
+  --publisher Microsoft.Azure.Extensions \
+  --settings '{"commandToExecute": "docker run -d --restart=unless-stopped -e RANCHER_ACCESS_KEY=02579406B155C15055D0 -e RANCHER_SECRET_KEY=hmtAU1QhzssELTi3pJdarzFK9GxRN2FU5hUihSCh -e RANCHER_URL=http://172.21.1.196:8080/v2-beta/schemas -e RANCHER_ENVIRONMENT=1a7 -e INTERVAL=30 catwarrior/rancher-purge"}'
