bfb47936e7261176f02da165e4033493d27dab3b
catwarrior <lishufan00@aliyun.com>
2017-06-01 08:55:19 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /etc/docker
-touch /etc/docker/daemon.json
-echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"] } > /etc/docker/daemon.json
-systemctl enable docker.service
-systemctl restart docker.service
