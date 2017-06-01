0d78b522debd3d52edb454aba19c28fa966de923
catwarrior <lishufan00@aliyun.com>
2017-06-01 08:08:36 +0800

    Update rancher-slaves.sh

--- v1/rancher-slaves.sh
+++ v1/rancher-slaves.sh
@@ -4,9 +4,9 @@ touch /etc/docker/daemon.json
 echo { \"registry-mirrors\": [\"https://iakbs8nw.mirror.aliyuncs.com\"], \"insecure-registries\": [\"42.159.29.145:18083\"], \"storage-driver\": \"overlay2\" } > /etc/docker/daemon.json
 systemctl enable docker.service
 systemctl restart docker.service
-vmname=`cat /proc/sys/kernel/random/uuid| cksum | cut -f1 -d" "`
-hostname vm$vmname
-sysctl -w kernel.hostname=vm$vmname
+##vmname=`cat /proc/sys/kernel/random/uuid| cksum | cut -f1 -d" "`
+##hostname vm$vmname
+##sysctl -w kernel.hostname=vm$vmname
 ## add nodes
 docker run --rm --privileged \
   -v /var/run/docker.sock:/var/run/docker.sock \
